828e6bbb6c2232b781c1a869ad51f90a4bc49b0e
ShubhamMayenkar <shubhammayekar14@gmail.com>
2019-11-20 16:36:00 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,46 +1,46 @@
 jQuery(document).ready(function($){
 
-  jQuery('.hamburger').click(function(){
-    $('.main_container').toggleClass('sidebar_sm');
-  });
-
-  jQuery('.link_signup').click(function(){
-    console.log("signup link clicked");
-    $('.login').addClass('signup_view');
-  });
-
-  jQuery('.link_login').click(function(){
-    if ($('.login').hasClass('signup_view')) {
-      $('.login').removeClass('signup_view');
-    }
-    else {
-
-    }
-  });
-
-  jQuery(".add_product_btn").click(function(){
-    jQuery("#addProductModal").modal("show");
-  });
-
-  jQuery(".edit_product_btn").click(function(){
-    jQuery("#updateProductModal").modal("show");
-  });
-
-  function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                $("#select-image").css("background-image", "url(" + e.target.result + ")");
-                $("#select-image .select_image_overlay").addClass("remove_overlay");
-            }
-
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-
-    $("#imgInp").change(function(){
-        readURL(this);
+    jQuery('.hamburger').click(function(){
+      $('.main_container').toggleClass('sidebar_sm');
     });
-
-});
+  
+    jQuery('.link_signup').click(function(){
+      console.log("signup link clicked");
+      $('.login').addClass('signup_view');
+    });
+  
+    jQuery('.link_login').click(function(){
+      if ($('.login').hasClass('signup_view')) {
+        $('.login').removeClass('signup_view');
+      }
+      else {
+  
+      }
+    });
+  
+    jQuery(".add_product_btn").click(function(){
+      jQuery("#addProductModal").modal("show");
+    });
+  
+    jQuery(".edit_product_btn").click(function(){
+      jQuery("#updateProductModal").modal("show");
+    });
+  
+    function readURL(input) {
+          if (input.files && input.files[0]) {
+              var reader = new FileReader();
+  
+              reader.onload = function (e) {
+                  $("#select-image").css("background-image", "url(" + e.target.result + ")");
+                  $("#select-image .select_image_overlay").addClass("remove_overlay");
+              }
+  
+              reader.readAsDataURL(input.files[0]);
+          }
+      }
+  
+      $("#imgInp").change(function(){
+          readURL(this);
+      });
+  
+  });
